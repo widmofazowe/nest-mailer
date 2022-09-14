e8d2dcfc80fb71dfd46f49e7629e44890c9b6240
widmo <widomski.pawel@gmail.com>
2022-09-14 21:25:00 +0200

    update module

--- src/mailer.module.ts
+++ src/mailer.module.ts
@@ -1,7 +1,7 @@
 import { DynamicModule, Module, Type } from '@nestjs/common';
 import { MandrillMailer } from './clients/mandrill.service';
-import { MAILER, SendEmailListener } from './listeners/send-email.listener';
-import { Mailer } from './mailer.service';
+import { SendEmailListener } from './listeners/send-email.listener';
+import { MAILER, Mailer } from './mailer.service';
 
 interface ModuleProps {
   mailer: Type<Mailer>;
