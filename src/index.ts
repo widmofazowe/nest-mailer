b44e44b9d11b2eb4bd9c4fe1eec1b449a25ef35e
widmo <widomski.pawel@gmail.com>
2022-09-20 12:42:34 +0200

    update index

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { MailerModule } from './mailer.module';
 export { MandrillMailer } from './clients/mandrill.service';
+export { SendgridMailer } from './clients/sendgrid.service';
 export { SendEmailEvent, SEND_EMAIL_EVENT } from './listeners/send-email.event';
